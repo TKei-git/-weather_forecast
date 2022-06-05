fc0b90cd880e75bddced61c96e4f84620fc79d83
TKei-git <toxaie3@gmail.com>
2022-06-05 23:10:39 +0900

    バッチ作成

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,9 @@ COPY . /myapp
 # Add a script to be executed every time the container starts.
 COPY entrypoint.sh /usr/bin/
 RUN chmod +x /usr/bin/entrypoint.sh
+RUN apt-get update -qq && \
+    apt-get install -y cron
+
 ENTRYPOINT ["entrypoint.sh"]
 EXPOSE 3000
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,26 +1,23 @@
-set :environment, "development"
+# whenever 定期実行
+# rake 'ファイル名':タスク名'
+# Rails.root(Railsメソッド)を使用するために必要
+require File.expand_path(File.dirname(__FILE__) + '/environment')
 
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# .zshrcとrbenvのパスを指定するrakeを定義
+job_type :rake, "source /Users/docha/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
-every 1.minute do
-    runner "Batch::TestBatch.testBatch"
+# cronを実行する環境変数(:development, :product, :test)
+# 環境変数'RAILS_ENV'にセットされている変数またはdevelopmentを指定
+rails_env = ENV['RAILS_ENV'] || :development
+
+# cronを実行する環境変数をセット
+set :environment, rails_env
+ENV.each { |k, v| env(k, v) } # これを追加
+
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
+
+# 一時間毎に実行
+every 1.day, at: '5:30 am' do
+  rake 'weathers:get_data', :environment => :development
 end
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
